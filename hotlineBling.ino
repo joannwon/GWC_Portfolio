525a5188d592f0b9705b552a0ba2d9de007ce7c4
joannwon <joannwon0123210@gmail.com>
2017-07-12 09:24:58 -0700

    Add files via upload

---
+++ hotlineBling.ino
@@ -0,0 +1,31 @@
+#include "pitches.h"
+
+
+int melody[] = {
+  NOTE_D4, NOTE_D4, NOTE_D4, NOTE_F4, NOTE_E4, NOTE_D4, NOTE_C4, NOTE_E4, NOTE_C4, 0, 0, NOTE_F4, NOTE_E4, NOTE_D4, NOTE_C4, NOTE_E4, NOTE_C4, NOTE_A3, NOTE_F4, NOTE_E4, NOTE_D4, NOTE_C4, NOTE_D4,
+  NOTE_C5, NOTE_C5, NOTE_D5, 0, 0, NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_G4, NOTE_G4, NOTE_A4, 0, 0, NOTE_C5, NOTE_C5, NOTE_A4, NOTE_A4, NOTE_C5, NOTE_C5, NOTE_D5, 0, 0, NOTE_G4, NOTE_A4, NOTE_G4, 
+  NOTE_F4, NOTE_G4, NOTE_G4, NOTE_A4, 0, NOTE_C5, NOTE_A4, NOTE_C5, NOTE_A4, NOTE_C5, NOTE_A4, NOTE_C5  
+};
+
+int noteDurations[] = {
+  4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 2, 4, 4, 4, 4, 2, 2, 2, 4, 4, 4, 4, 4, 2, 2, 2, 4, 4, 4, 4, 4, 4, 4, 2, 2, 2, 4, 4, 4, 4, 4, 4, 2, 2, 2, 4, 4, 4, 4, 4, 4, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4
+};
+
+void setup() {
+
+  
+while(true){
+  for (int thisNote = 0; thisNote <60; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+
+  }
+}
+}
+
+void loop() {
+  // no need to repeat the melody.
+}
---
+++ piezo.ino
@@ -0,0 +1,26 @@
+//#include "pitches.h"
+//
+//int melody[] = {
+//  NOTE_C4, NOTE_C4, NOTE_D4, NOTE_E4, NOTE_C4, NOTE_E4, NOTE_D4, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_E4, NOTE_C4, NOTE_B3, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4,
+//  NOTE_E4, NOTE_D4, NOTE_C4, NOTE_B3, NOTE_G3, NOTE_A3, NOTE_B3, NOTE_C4, NOTE_C4, NOTE_A3, NOTE_B3, NOTE_A3, NOTE_G3, NOTE_A3, NOTE_B3, NOTE_C4
+//};
+//
+//int noteDurations[] = {
+//  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 
+//};
+//
+//void setup() {
+//
+//  for (int thisNote = 0; thisNote <34; thisNote++) {
+//    int noteDuration = 1000 / noteDurations[thisNote];
+//    tone(8, melody[thisNote], noteDuration);
+//
+//    int pauseBetweenNotes = noteDuration * 1.30;
+//    delay(pauseBetweenNotes);
+//
+//  }
+//}
+//
+//void loop() {
+//  // no need to repeat the melody.
+//}
